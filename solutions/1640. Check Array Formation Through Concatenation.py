bae1968a3790a104021be5d428d03758673dcf67
Udhav Sethi <udhavsethi@users.noreply.github.com>
2020-11-25 13:22:23 -0500

    48 ms

--- solutions/1640. Check Array Formation Through Concatenation.py
+++ solutions/1640. Check Array Formation Through Concatenation.py
@@ -4,10 +4,9 @@ class Solution:
         for i in range(len(arr)):
             for sublist in pieces:
                 if sublist[0] == arr[i]:     # match found with first element of a sublist
-                    compareList.append(sublist)
+                    compareList.extend(sublist)
                     i += len(sublist)
                     break
-        flat_list = [item for sublist in compareList for item in sublist]
-        if flat_list == arr:
+        if compareList == arr:
             return True
         return False
