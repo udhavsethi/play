9fdfa86034a4402797afac3fa284178f78fdddfc
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-05-13 08:53:24 -0400

    N/A

--- solutions/942. DI String Match.py
+++ solutions/942. DI String Match.py
@@ -20,3 +20,21 @@ class Solution:
                     perm[j-1] += 1
                     j -= 1
         return perm
+​
+    '''
+    Optimal solution
+    
+    class Solution(object):
+    def diStringMatch(self, S):
+        lo, hi = 0, len(S)
+        ans = []
+        for x in S:
+            if x == 'I':
+                ans.append(lo)
+                lo += 1
+            else:
+                ans.append(hi)
+                hi -= 1
+​
+        return ans + [lo]
+    '''
