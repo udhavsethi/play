ff3463cf3716d1dc973079f702cf0b52593630f2
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-05-18 06:01:11 -0400

    96 ms

--- solutions/1047. Remove All Adjacent Duplicates In String.py
+++ solutions/1047. Remove All Adjacent Duplicates In String.py
@@ -6,6 +6,7 @@ class Solution:
                 left = i if i>=0 else i+2
                 right = i+2 if i<(len(s)-2) else i
                 s = s[:left] + s[right:] if left != right else s[:left]
-                i = -1
+                # move to previous index as it may now contribute toward duplicates
+                i = i-2 if i-1 >= 0 else i-1
             i += 1
         return s
