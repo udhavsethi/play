5a515776b76c92f33fb8462a2c7277de7960370f
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-09-28 18:53:46 -0700

    56 ms

--- solutions/1. Two Sum.py
+++ solutions/1. Two Sum.py
@@ -1,7 +1,17 @@
+# review
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n):
-            for j in range(n):
-                if (i != j) and (nums[i] + nums[j] == target):
-                    return [i, j]
+        record = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in record:
+                return [i, record[diff]]
+            record[nums[i]] = i
+​
+    ### brute force
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     n = len(nums)
+    #     for i in range(n):
+    #         for j in range(n):
+    #             if (i != j) and (nums[i] + nums[j] == target):
+    #                 return [i, j]
