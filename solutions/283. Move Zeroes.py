b5f90bf8f4d8e094deebf404198f04dd58e2fa1c
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-09-28 22:35:00 -0700

    N/A

---
+++ solutions/283. Move Zeroes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def moveToBack(self, nums: List[int], index, length):
+        while (index+1) < length:
+            if nums[index+1] != 0:
+                nums[index], nums[index+1] = nums[index+1], nums[index]
+            index += 1
+        return nums
+        
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        right = n-2
+        while right >= 0:
+            if nums[right] == 0:
+                nums = self.moveToBack(nums, right, n)
+            right = right - 1
