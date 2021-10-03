07053188f879e30255bdfdd06a4db5605061fd24
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-03 16:28:27 -0700

    52 ms

--- solutions/242. Valid Anagram.py
+++ solutions/242. Valid Anagram.py
@@ -1,3 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if not len(s) == len(t):
+            return False
+        count = {}
+        for char in s:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+        
+        for tchar in t:
+            if tchar in count:
+                count[tchar] -= 1
+                if count[tchar] < 0:
+                    return False
+            else:
+                return False
+        return True
+​
+    # def isAnagram(self, s: str, t: str) -> bool:
+    #     return sorted(s) == sorted(t)
