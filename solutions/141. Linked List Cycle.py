c45e26df17edb5b16c642040ff835806986b7128
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-04 23:15:10 -0700

    118 ms

--- solutions/141. Linked List Cycle.py
+++ solutions/141. Linked List Cycle.py
@@ -4,14 +4,29 @@
 #         self.val = x
 #         self.next = None
 ​
+# review
+​
+# using two pointers
 class Solution:
     def hasCycle(self, head: ListNode) -> bool:
-        visited = set()
-        curr = head
-        while curr is not None:
-            if curr in visited:
-                return True
-            else:
-                visited.add(curr)
-            curr = curr.next
-        return False
+        slow, fast = head, head
+        try:
+            while fast is not None:
+                slow = slow.next
+                fast = fast.next.next
+                if slow == fast:
+                    return True
+        except:
+            return False
+​
+# class Solution:
+#     def hasCycle(self, head: ListNode) -> bool:
+#         visited = set()
+#         curr = head
+#         while curr is not None:
+#             if curr in visited:
+#                 return True
+#             else:
+#                 visited.add(curr)
+#             curr = curr.next
+#         return False
