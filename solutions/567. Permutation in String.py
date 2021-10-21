7708a20db240ea7b04301f551f59d619fab95eef
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-20 18:28:57 -0700

    388 ms

--- solutions/567. Permutation in String.py
+++ solutions/567. Permutation in String.py
@@ -1,40 +1,51 @@
+# review
+# also: check time complexities in solutions
 class Solution:
     
-    def get_counts(self, s1: str) -> dict:
-        s1_chars = {}
-        for c in s1:
-            if c in s1_chars:
-                s1_chars[c] += 1
+    def get_counts(self, st: str) -> dict:
+        st_chars = {}
+        for c in st:
+            if c in st_chars:
+                st_chars[c] += 1
             else:
-                s1_chars[c] = 1
-        return s1_chars
-    
+                st_chars[c] = 1
+        return st_chars
+​
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # dict containing all the chars in s1 and their count
-        s1_chars = self.get_counts(s1)
         s1len = len(s1)
         s2len = len(s2)
-        lastnomatch = -1
-        curr = 0
+        if s1len > s2len:
+            return False
 ​
-        while curr < s2len:
-            char = s2[curr]
-            if char in s1_chars:
-                # print("curr, char, s1_chars", curr, char, s1_chars)
-                if s1_chars[char] > 0:
-                    s1_chars[char] -= 1
-                    # check here
-                    if (curr - lastnomatch) == s1len:
-                        # print("curr, lastnomatch, s2[lastnomatch:curr]: ", curr, lastnomatch, s2[lastnomatch:curr])
-                        return True
-                    curr += 1
+        s1_chars = self.get_counts(s1)
+​
+        s2len = len(s2)
+        s2_chars = None
+        for curr in range(0, s2len-s1len+1):
+            # print("curr ", curr)
+            if s2_chars is not None:
+                if s2[curr+s1len-1] in s2_chars:                    
+                    s2_chars[s2[curr+s1len-1]] += 1
                 else:
-                    # restart counting
-                    lastnomatch += 1
-                    curr = lastnomatch+1
-                    s1_chars = self.get_counts(s1)
+                    s2_chars[s2[curr+s1len-1]] = 1
+                s2_chars[s2[curr-1]] -= 1
             else:
-                lastnomatch = curr
-                curr += 1
-                s1_chars = self.get_counts(s1)
+                s2_chars = self.get_counts(s2[curr:curr+s1len])
+            # check
+            if set([(k,v) for k,v in s1_chars.items() if v>0]) == set([(k,v) for k,v in s2_chars.items() if v>0]):
+                    return True
         return False
+​
+'''
+class Solution:
+    
+    def get_counts(self, s1: str) -> dict:
+        s1_chars = {}
+        for c in s1:
+            if c in s1_chars:
+                s1_chars[c] += 1
+            else:
+                s1_chars[c] = 1
+        return s1_chars
+    
+    def checkInclusion(self, s1: str, s2: str) -> bool:
