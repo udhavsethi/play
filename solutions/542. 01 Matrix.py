fbaddc9d68221489ad4aa3c5fd053988a1693ea2
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-25 16:41:23 -0700

    N/A

--- solutions/542. 01 Matrix.py
+++ solutions/542. 01 Matrix.py
@@ -1,30 +1,27 @@
-# work in progress
+# works but time limit exceeded
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         rows, cols = len(mat), len(mat[0])
 ​
-        def dfs(row, col):
+        def dfs1(row, col):
             if row<0 or row>=rows or col<0 or col>=cols:
-                return None
+                return float("inf")
             if mat[row][col] == 0:
                 return 0
-            if visited[row][col] == 1:
-                return values[row][col]
-            visited[row][col] = 1
-            distances = [dfs(row-1, col),
-                         dfs(row+1,col),
-                         dfs(row,col-1),
-                         dfs(row,col+1)]
-            return 1 + min([x for x in distances if x is not None])
+            return 1 + min(dfs1(row-1, col), dfs1(row,col-1))
+        
+        def dfs2(row, col):
+            if row<0 or row>=rows or col<0 or col>=cols:
+                return float("inf")
+            if mat[row][col] == 0:
+                return 0
+            return min(mat[row][col], 1 + min(dfs2(row+1,col), dfs2(row,col+1)))
 ​
-        visited = [[0 for i in range(cols)] for j in range(rows)]
-        values = [[0 for i in range(cols)] for j in range(rows)]
         for r in range(rows):
             for c in range(cols):
-                values[r][c] = dfs(r,c)
-        visited = [[0 for i in range(cols)] for j in range(rows)]
+                mat[r][c] = dfs1(r,c)
         for r in reversed(range(rows)):
             for c in reversed(range(cols)):
-                values[r][c] = dfs(r,c)
+                mat[r][c] = dfs2(r,c)
         return mat
 ​
