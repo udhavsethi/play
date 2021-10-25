32e8f22c7334a4a0da2aca9cd106b5fabf61ab59
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-24 17:42:24 -0700

    133 ms

--- solutions/733. Flood Fill.py
+++ solutions/733. Flood Fill.py
@@ -1,3 +1,5 @@
+# review
+# check recursive method in solutions
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
         stack = []
