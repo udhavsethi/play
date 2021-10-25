d6d002329aa58203b19462563d1adb5d5e97a706
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-24 17:24:44 -0700

    133 ms

--- solutions/733. Flood Fill.py
+++ solutions/733. Flood Fill.py
@@ -1,16 +1,19 @@
 class Solution:
-    # need to mark visited nodes!
-    def floodFillIn(self, image: List[List[int]], sr: int, sc: int, newColor: int, oldColor: int) -> List[List[int]]:
-        rows, cols = len(image), len(image[0])
-        for r in range(rows):
-            if image[r][sc] == oldColor:
-                image[r][sc] = newColor
-                self.floodFillIn(image, r, sc, newColor, oldColor)
-        for c in range(cols):
-            if image[sr][c] == image[sr][sc]:
-                image[sr][c] = newColor
-                self.floodFillIn(image, sr, c, newColor, oldColor)
-        return image
-​
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        return self.floodFillIn(image, sr, sc, newColor, image[sr][sc])
+        stack = []
+        rows, cols = len(image), len(image[0])
+        oldColor = image[sr][sc]
+        if oldColor == newColor:
+            return image
+​
+        image[sr][sc] = newColor
+        stack.append((sr,sc))
+​
+        while len(stack) != 0:
+            (cr,cc) = stack.pop()
+            adjacent_pixels = [(cr+1, cc), (cr-1, cc), (cr, cc+1), (cr, cc-1)]
+            for r,c in adjacent_pixels:
+                if r>=0 and r<rows and c>=0 and c<cols and (image[r][c] == oldColor):
+                    image[r][c] = newColor
+                    stack.append((r,c))
+        return image
