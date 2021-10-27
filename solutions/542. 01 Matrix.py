5bafacc606b9f76fbadd59355bb8d67e646e47d1
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-26 18:00:06 -0700

    1388 ms

--- solutions/542. 01 Matrix.py
+++ solutions/542. 01 Matrix.py
@@ -1,27 +1,43 @@
-# works but time limit exceeded
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
         rows, cols = len(mat), len(mat[0])
+        visited = [[0 for i in range(cols)] for j in range(rows)]
+        res = [[float(inf) for i in range(cols)] for j in range(rows)]
+        queue = []
 ​
-        def dfs1(row, col):
-            if row<0 or row>=rows or col<0 or col>=cols:
-                return float("inf")
-            if mat[row][col] == 0:
-                return 0
-            return 1 + min(dfs1(row-1, col), dfs1(row,col-1))
-        
-        def dfs2(row, col):
-            if row<0 or row>=rows or col<0 or col>=cols:
-                return float("inf")
-            if mat[row][col] == 0:
-                return 0
-            return min(mat[row][col], 1 + min(dfs2(row+1,col), dfs2(row,col+1)))
-​
+        # add zeros to the queue
         for r in range(rows):
             for c in range(cols):
-                mat[r][c] = dfs1(r,c)
-        for r in reversed(range(rows)):
-            for c in reversed(range(cols)):
-                mat[r][c] = dfs2(r,c)
-        return mat
+                if mat[r][c] == 0:
+                    queue.append((r,c))
+                    res[r][c] = 0
+                    visited[r][c] = 1
+                    
+        # dequeue cell and enqueue adjacent cells - BFS
+        while len(queue) != 0:
+            cr, cc = queue.pop(0)
+            visited[cr][cc] = 1
+            distances = []
+            # find neighbours of cur cell and add to the queue
+            if cr > 0:
+                distances.append(res[cr-1][cc])
+                if visited[cr-1][cc] == 0:
+                    queue.append((cr-1, cc))
+            if cr < rows-1:
+                distances.append(res[cr+1][cc])
+                if visited[cr+1][cc] == 0:
+                    queue.append((cr+1, cc))
+            if cc > 0:
+                distances.append(res[cr][cc-1])
+                if visited[cr][cc-1] == 0:
+                    queue.append((cr, cc-1))
+            if cc < cols-1:
+                distances.append(res[cr][cc+1])
+                if visited[cr][cc+1] == 0:
+                    queue.append((cr, cc+1))
+            if res[cr][cc] == float(inf):
+                res[cr][cc] = 1 + min(distances)
+        return res
 ​
+# works but time limit exceeded
+# class Solution:
