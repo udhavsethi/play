f6175f1315cb69c80810740ce532118ad7b15bcb
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-27 18:32:18 -0700

    36 ms

--- solutions/206. Reverse Linked List.py
+++ solutions/206. Reverse Linked List.py
@@ -3,23 +3,28 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+​
+# recursive solution
 class Solution:
+    def reverse(self, node):
+        if node.next is None:
+            return node
+        prev = self.reverse(node.next)
+        prev.next = node
+        return node
+​
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
+        if head is None:
             return head
-        elif not head.next.next:
-            temp = head.next
-            head.next.next = head
-            head.next = None
-            return temp
+        newhead = head
+        while newhead.next is not None:
+            newhead = newhead.next
+        self.reverse(head)
+        head.next = None
+        return newhead
 ​
-        first, second, third = head, head.next, head.next.next
-        first.next = None
-        while third is not None:
-            second.next = first
-            first = second
-            second = third
-            third = third.next
-        second.next = first
-        return second
 ​
+​
+# iterative solution
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
