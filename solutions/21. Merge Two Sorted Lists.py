65e011e435953765de391b3da4a5005f4a988656
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-27 19:29:16 -0700

    40 ms

--- solutions/21. Merge Two Sorted Lists.py
+++ solutions/21. Merge Two Sorted Lists.py
@@ -3,27 +3,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+​
+​
+# recursive
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if l1 is None:
             return l2
         elif l2 is None:
             return l1
-​
-        if l1.val >= l2.val:
-            head, l2 = l2, l2.next
+        if l1.val <= l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
         else:
-            head, l1 = l1, l1.next
-        headref = head
-        while l1 is not None and l2 is not None:
-            if l1.val >= l2.val:
-                head.next = l2
-                l2 = l2.next
-            else:
-                head.next = l1
-                l1 = l1.next
-            head = head.next
-        head.next = l2 if l1 is None else l1
-        return headref
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
+​
+​
+# iterative
+# class Solution:
+#     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+#         if l1 is None:
+#             return l2
+#         elif l2 is None:
+#             return l1
+​
+#         if l1.val >= l2.val:
+#             head, l2 = l2, l2.next
+#         else:
+#             head, l1 = l1, l1.next
+#         headref = head
+#         while l1 is not None and l2 is not None:
+#             if l1.val >= l2.val:
+#                 head.next = l2
+#                 l2 = l2.next
+#             else:
+#                 head.next = l1
+#                 l1 = l1.next
+#             head = head.next
+#         head.next = l2 if l1 is None else l1
+#         return headref
             
         
