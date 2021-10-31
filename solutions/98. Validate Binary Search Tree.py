db91703f7d298cf338cc64ae0bc5be17586fe910
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-31 12:37:34 -0700

    132 ms

--- solutions/98. Validate Binary Search Tree.py
+++ solutions/98. Validate Binary Search Tree.py
@@ -4,20 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# wrong solution
 class Solution:
+    def maxValue(self, root):
+        if not root.left and not root.right:
+            return root.val
+        elif not root.left:
+            return max(root.val, self.maxValue(root.right))
+        elif not root.right:
+            return max(root.val, self.maxValue(root.left))
+        else:
+            return max(root.val, self.maxValue(root.left), self.maxValue(root.right))
+    
+    def minValue(self, root):
+        if not root.left and not root.right:
+            return root.val
+        elif not root.left:
+            return min(root.val, self.minValue(root.right))
+        elif not root.right:
+            return min(root.val, self.minValue(root.left))
+        else:
+            return min(root.val, self.minValue(root.left), self.minValue(root.right))
+​
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         if root == None or (not root.left and not root.right):
             return True
         elif not root.left:
-            return (root.right.val > root.val) and self.isValidBST(root.right)
+            return (self.minValue(root.right) > root.val) and self.isValidBST(root.right)
         elif not root.right:
-            return (root.left.val < root.val) and self.isValidBST(root.left)
+            return (self.maxValue(root.left) < root.val) and self.isValidBST(root.left)
         else:
             return (
-                (root.left.val < root.val)
-                and (root.right.val > root.val)
+                (self.maxValue(root.left) < root.val)
+                and (self.minValue(root.right) > root.val)
                 and self.isValidBST(root.left)
                 and self.isValidBST(root.right)
-            )
-        return False
