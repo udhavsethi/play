0ba289901cc80cf2709780d568208d785d42db96
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-10-31 16:43:20 -0700

    32 ms

--- solutions/198. House Robber.py
+++ solutions/198. House Robber.py
@@ -1,9 +1,22 @@
 class Solution:
-    # simple recursive solution - TLE
-    def robAt(self, nums, i):
+    # review
+    # recursive solution with DP
+    def robAt(self, nums, i, mem):
         if i < 0:
             return 0
-        return max(self.robAt(nums, i-2) + nums[i], self.robAt(nums, i-1))
+        if i in mem:
+            return mem[i]
+        mem[i] = max(self.robAt(nums, i-2, mem) + nums[i], self.robAt(nums, i-1, mem))
+        return mem[i]
 ​
     def rob(self, nums: List[int]) -> int:
-        return self.robAt(nums, len(nums)-1)
+        return self.robAt(nums, len(nums)-1, {})
+    
+    # simple recursive solution - TLE
+#     def robAt(self, nums, i):
+#         if i < 0:
+#             return 0
+#         return max(self.robAt(nums, i-2) + nums[i], self.robAt(nums, i-1))
+​
+#     def rob(self, nums: List[int]) -> int:
+#         return self.robAt(nums, len(nums)-1)
