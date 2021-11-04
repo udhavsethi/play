a6f742d730d768f4cf1df44fc3e1ea70458cd298
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-11-03 17:45:34 -0700

    80 ms

--- solutions/700. Search in a Binary Search Tree.py
+++ solutions/700. Search in a Binary Search Tree.py
@@ -6,9 +6,7 @@
 #         self.right = right
 class Solution:
     def searchBST(self, root: TreeNode, val: int) -> TreeNode:
-        if root == None:
-            return None
-        elif root.val == val:
+        if root == None or root.val == val:
             return root
         elif root.val < val:
             return self.searchBST(root.right, val)
