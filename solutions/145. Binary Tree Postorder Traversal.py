eaab277ee25b2bd5706d26916c121c04b2dafe8d
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-11-05 10:57:24 -0700

    52 ms

--- solutions/145. Binary Tree Postorder Traversal.py
+++ solutions/145. Binary Tree Postorder Traversal.py
@@ -6,6 +6,18 @@
 #         self.right = right
 # review
 # iterative solutions - https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45551/Preorder-Inorder-and-Postorder-Iteratively-Summarization
+​
+# time complexity
+# In-order, Pre-order, and Post-order traversals are Depth-First traversals.
+​
+# For a Graph, the complexity of a Depth First Traversal is O(n + m), where n is the number of nodes, and m is the number of edges.
+​
+# Since a Binary Tree is also a Graph, the same applies here. The complexity of each of these Depth-first traversals is O(n+m).
+​
+# Since the number of edges that can originate from a node is limited to 2 in the case of a Binary Tree, the maximum number of total edges in a Binary Tree is n-1, where n is the total number of nodes.
+​
+# The complexity then becomes O(n + n-1), which is O(n).
+​
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         if root == None:
