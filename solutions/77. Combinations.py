a24addb48fb30c5ec2450c7c682a04e18a4501a2
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-11-15 14:28:10 -0800

    76 ms

--- solutions/77. Combinations.py
+++ solutions/77. Combinations.py
@@ -5,8 +5,10 @@ class Solution:
             return [[x for x in range(1,n+1)]]
         if k == 1:
             return [[x] for x in range(1,n+1)]
+        # (n-1)C(k-1)
         res = self.combine(n-1, k-1)
         for lst in res:
-            lst = lst.append(n)
+            lst.append(n)
+        # (n-1)C(k)
         res.extend(self.combine(n-1, k))
         return res
