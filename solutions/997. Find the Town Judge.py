ee186011e87686a35a37b1a7e5bcd5e41dea3489
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-11-21 14:57:46 -0800

    1044 ms

---
+++ solutions/997. Find the Town Judge.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        score = [0]*n
+        for entry in trust:
+            truster, trustee = entry[0], entry[1]
+            score[trustee-1] += 1
+            score[truster-1] -= 1
+        for idx in range(len(score)):
+            if score[idx] == n-1:
+                return idx+1
+        return -1
