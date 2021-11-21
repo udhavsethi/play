05e746eb5290999dbc95ac6cbd79e3c8b62b860b
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-11-21 15:25:26 -0800

    1589 ms

--- solutions/1557. Minimum Number of Vertices to Reach All Nodes.py
+++ solutions/1557. Minimum Number of Vertices to Reach All Nodes.py
@@ -1,30 +1,29 @@
-# works, but TLE
+# review
+# just return the nodes with zero in-edges
 class Solution:
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        # first use visited to check nodes with 0 incoming edges
-        visited = [0]*n
-        for [src, tgt] in edges:
-            visited[tgt] += 1
-        stack = []
+        in_edges = [0]*n
         res = []
-        # now use prev info from visited and update it to track the visited nodes
-        for i in range(len(visited)):
-            if visited[i] == 0:
-                stack.append(i)
-                res.append(i)
-                visited[i] = 1
-            else:
-                visited[i] = 0
-​
-        while len(stack) != 0:
-            el = stack.pop()
-            visited[el] = 1
-            for [src, tgt] in edges:
-                if src == el:
-                    stack.append(tgt)
-        
-        # check if anything is not visited
-        for i in range(len(visited)):
-            if visited[i] == 0:
+        for [src, tgt] in edges:
+            in_edges[tgt] += 1
+        for i in range(len(in_edges)):
+            if in_edges[i] == 0:
                 res.append(i)
         return res
+​
+​
+# # works, but TLE
+# class Solution:
+#     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
+#         # first use visited to check nodes with 0 incoming edges
+#         visited = [0]*n
+#         for [src, tgt] in edges:
+#             visited[tgt] += 1
+#         stack = []
+#         res = []
+#         # now use prev info from visited and update it to track the visited nodes
+#         for i in range(len(visited)):
+#             if visited[i] == 0:
+#                 stack.append(i)
+#                 res.append(i)
+#                 visited[i] = 1
