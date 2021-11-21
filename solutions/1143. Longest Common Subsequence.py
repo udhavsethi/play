acb25cdbc1a2dc6be3e0fc444a3744d72b229b06
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-11-21 13:24:50 -0800

    1800 ms

--- solutions/1143. Longest Common Subsequence.py
+++ solutions/1143. Longest Common Subsequence.py
@@ -1,12 +1,35 @@
-# simple recursion - TLE
+# review
+# DP
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    def longestCommonSubsequenceWithMem(self, text1, text2, mem):
         if text1 == "" or text2 == "":
             return 0
         if text1 == text2:
             return len(text1)
+        if (text1, text2) in mem:
+            return mem[(text1, text2)]
         if text1[0] == text2[0]:
-            return 1 + self.longestCommonSubsequence(text1[1:], text2[1:])
+            mem[(text1, text2)] = 1 + self.longestCommonSubsequenceWithMem(text1[1:], text2[1:], mem)
+            return mem[(text1, text2)]
         else:
-            return max(self.longestCommonSubsequence(text1[1:], text2),
-                      self.longestCommonSubsequence(text1, text2[1:]))
+            mem[(text1, text2)] = max(self.longestCommonSubsequenceWithMem(text1[1:], text2, mem),
+                                      self.longestCommonSubsequenceWithMem(text1, text2[1:], mem))
+            return mem[(text1, text2)]
+​
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        mem = {}
+        return self.longestCommonSubsequenceWithMem(text1, text2, mem)
+        
+​
+# # simple recursion - TLE
+# class Solution:
+#     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+#         if text1 == "" or text2 == "":
+#             return 0
+#         if text1 == text2:
+#             return len(text1)
+#         if text1[0] == text2[0]:
+#             return 1 + self.longestCommonSubsequence(text1[1:], text2[1:])
+#         else:
+#             return max(self.longestCommonSubsequence(text1[1:], text2),
+#                       self.longestCommonSubsequence(text1, text2[1:]))
