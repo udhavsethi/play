93e4d18ffaac36e7e23350e0e3a17c9466640b01
Udhav Sethi <udhavsethi@users.noreply.github.com>
2021-11-23 10:49:39 -0800

    292 ms

--- solutions/189. Rotate Array.py
+++ solutions/189. Rotate Array.py
@@ -9,6 +9,5 @@ class Solution:
             nums[:] = nums[-k:] + nums[:(n-k)]
 ​
         # review
-        # don't know why doesn't work:
-        # n = len(nums)
-        # nums[:] = [nums[(i+k+1)%n] for i in range(n)]
+        # O(1) space
+        # The basic idea is that, for example, nums = [1,2,3,4,5,6,7] and k = 3, first we reverse [1,2,3,4], it becomes[4,3,2,1]; then we reverse[5,6,7], it becomes[7,6,5], finally we reverse the array as a whole, it becomes[4,3,2,1,7,6,5] ---> [5,6,7,1,2,3,4].
